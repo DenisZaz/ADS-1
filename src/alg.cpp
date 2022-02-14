ecee78600ee0b958dd155010a2ff5e5eb155434c
Зазунов Денис <99178404+DenisZaz@users.noreply.github.com>
2022-02-14 23:58:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,49 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value == 1) {
+    return false;
+}
+for (int i = 2; i <= sqrt(value); i++) {
+  if (value % i == 0) {
+    return false;
+    break;
+  }
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+int i = 0;
+int num = 2;
+  while (n >= i) {
+    if (checkPrime(num)) {
+      i++;
+    }
+    if (n == i) {
+      return num;
+    }
+    num++;
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+  while (true) {
+    value++;
+    if (checkPrime(value)) {
+      return value;
+      break;
+    }
+  }
 }
 
+
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t summ = 0;
+for (int i = 0; i < n; i++) {
+  if (checkPrime(i)) {
+    summ += i;
+  }
+}
+return summ;
 }
