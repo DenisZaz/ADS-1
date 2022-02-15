84417cb063ad9a8c65eb7b2c390a25a65755f80a
Зазунов Денис <99178404+DenisZaz@users.noreply.github.com>
2022-02-15 17:32:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,13 +5,11 @@
 
 bool checkPrime(uint64_t value) {
 if (value == 1) {
-    return false;
+  return false;
 }
-int sqrtValue = sqrt(value);
-for (int i = 2; i <= sqrtValue; i++) {
+for (int i = 2; i <= std::sqrt(value); i++) {
   if (value % i == 0) {
     return false;
-    break;
   }
 }
 return true;
@@ -20,36 +18,33 @@ return true;
 uint64_t nPrime(uint64_t n) {
 int i = 0;
 int num = 2;
-  while (n >= i) {
-    if (checkPrime(num)) {
-      i++;
-    }
-    num++;
-    if (n == i) {
-      return num;
-    }
+while (true) {
+  if (checkPrime(num)) {
+  i++;
   }
+  if (n == i) {
+    return num;
+  }
+  num++;
+}
+return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  while (true) {
-    value++;
-    if (checkPrime(value)) {
-      return value;
-      break;
-    }
-  }
+while (true) {
+  value++;
+  if (checkPrime(value)) 
+  return value;	
+}
+return value;
 }
 
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t summ = 0;
-int i = 0;
-  while (n > i) {
-    if (checkPrime(i)) {
-      summ += i;
-    }
-    i++;
-  }
-return summ;
+uint64_t sum = 0;
+for (int i = 0; i < hbound; i++) {
+  if (checkPrime(i))
+  sum += i;
+}
+return sum;
 }
